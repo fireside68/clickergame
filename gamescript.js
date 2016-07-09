0bb185ee1929d5cb5977435055a27ba59ae3ded1
Cedric Johnson <cjohns0913@gmail.com>
2016-07-09 16:50:27 -0400

    fixed a few more things; now it looks SOMEWHAT respectable :oncoming_bus:

--- gamescript.js
+++ gamescript.js
@@ -1,14 +1,14 @@
 /* global $ */
 
 $(document).ready(function () {
+  let score = +document.getElementById("mainscore").innerHTML
+  let clickScore = +document.getElementById("clickScore").innerHTML
     $('#clickButton').click(function accumulate() {
-        let score = +document.getElementById("mainscore").innerHTML
-        let clickScore = +document.getElementById("clickScore").innerHTML
-        if((score > 0) && (score % 100 === 0)) {
-          clickScore = ((score * 4) / 2) + 1
+        if((score > 0) && (score % 800 === 0)) {
+          clickScore = (score * 0.06) + 1
         }
         score += clickScore
-        document.getElementById("mainscore").innerHTML = score
-        document.getElementByID("clickScore").innerHTML = clickScore
+        document.getElementById("mainscore").innerHTML = score.toLocaleString()
+        document.getElementById("clickScore").innerHTML = clickScore.toLocaleString()
    })
 })
